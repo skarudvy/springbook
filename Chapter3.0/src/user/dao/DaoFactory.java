9ac95d8417fec09eb99a7ccdb879f7facb8b98e0
Nam <skarudvy0202@naver.com>
2013-01-11 07:30:27 +0900

    change

--- Chapter3.0/src/user/dao/DaoFactory.java
+++ Chapter3.0/src/user/dao/DaoFactory.java
@@ -8,7 +8,7 @@ import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 @Configuration
 public class DaoFactory {
-	@Bean
+	@Bean  
 	public DataSource dataSource() {
 		SimpleDriverDataSource dataSource = new SimpleDriverDataSource ();
 
