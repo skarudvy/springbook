df8bc338dba4665be8be52b6b66119586aea4366
Nam <skarudvy0202@naver.com>
2013-01-11 06:16:09 +0900

    commit test

--- Chapter3.0/src/learningtest/template/CalcSumTest.java
+++ Chapter3.0/src/learningtest/template/CalcSumTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-
+   
 public class CalcSumTest { 
 	Calculator calculator;
 	String numFilepath;
