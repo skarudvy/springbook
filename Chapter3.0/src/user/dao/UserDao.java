637e4dfcb361dcac744bd5c716fec5e3715daab7
Nam <skarudvy0202@naver.com>
2013-01-12 11:14:20 +0900

    코드 정리

--- Chapter3.0/src/user/dao/UserDao.java
+++ Chapter3.0/src/user/dao/UserDao.java
@@ -1,23 +1,20 @@
 package user.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
-
 import javax.sql.DataSource;
 
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
-
 import user.domain.User;
 
 public class UserDao {
+	public void setDataSource(DataSource dataSource) {
+		
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
 	private JdbcTemplate jdbcTemplate;
 	
 	private RowMapper<User> userMapper =
@@ -31,12 +28,6 @@ public class UserDao {
 				}
 			};
 	
-	
-	public void setDataSource(DataSource dataSource) {
-		
-		this.jdbcTemplate = new JdbcTemplate(dataSource);
-	}
-
 	public void add(final User user) throws SQLException {
 		this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)",
 				user.getId(), user.getName(), user.getPassword());
@@ -51,40 +42,13 @@ public class UserDao {
 	public void deleteAll() throws SQLException {
 		this.jdbcTemplate.update("delete from users");
 	}
-
 	
 	public List<User> getAll() {
 		return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
 	}
+	
 	public int getCount() throws SQLException  {
-//		return  this.jdbcTemplate.query(new PreparedStatementCreator() {
-//			
-//			@Override
-//			public PreparedStatement createPreparedStatement(Connection con)
-//					throws SQLException {
-//				return con.prepareStatement("select count(*) from users");
-//			}
-//		}, new ResultSetExtractor<Integer>() {
-//			public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
-//				rs.next();
-//				return rs.getInt(1);
-//			}
-//		});
-		
 		return jdbcTemplate.queryForInt("select count(*) from users");
 	}
-	
-//	private void executeSql(final String query) throws SQLException {
-//		this.jdbcContextWithStatementStrategy(
-//				new StatementStrategy() {
-//					public PreparedStatement makePreparedStatement(Connection c)
-//							throws SQLException {
-//					
-//						return c.prepareStatement(query);
-//					}
-//				}
-//			);
-//	}
-	
 	
 }
