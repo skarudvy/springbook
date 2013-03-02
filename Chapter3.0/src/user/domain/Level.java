ad232f5121f506b2546ab000972203c8365bdcc8
Nam <skarudvy0202@naver.com>
2013-03-02 13:47:47 +0900

    User Test add

--- Chapter3.0/src/user/domain/Level.java
+++ Chapter3.0/src/user/domain/Level.java
@@ -1,16 +1,24 @@
 package user.domain;
 
 public enum Level {
-	BASIC(1), SILVER(2), GOLD(3);
+	//enum 선언에 DB에 저장할 값과 함께 다음 단계의 레벨 정보도 추가한다.
+	GOLD(3, null), SILVER(2, GOLD), BASIC(1, SILVER);
 	
-	private final int value;	
+	private final int value;
+	//다음 단계의 레벨 정보를 스스로 갖고 있도록 Level 타입의 next 변수를 추가한다. 
+	private final Level next; 
 	
-	Level(int value) {
+	Level(int value, Level next) {
 		this.value = value;
+		this.next = next;
 	}
 	
 	public int intValue() {
 		return value;	
+	}
+	
+	public Level nextLevel() {
+		return this.next;
 	}
 	
 	public static Level valueOf(int value) {
---
+++ Chapter3.0/src/user/domain/UserTest.java
@@ -0,0 +1,37 @@
+package user.domain;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class UserTest {
+	User user;
+	
+	@Before
+	public void setUp() {
+		user = new User();
+	}
+	
+	@Test
+	public void upgradeLevel() {
+		Level[] levels = Level.values();
+		for(Level level : levels) {
+			if(level.nextLevel() == null ) continue;
+			user.setLevel(level);
+			user.upgradeLevel();
+			assertThat(user.getLevel(), is(level.nextLevel()));
+		}
+	}
+	
+	@Test(expected = IllegalStateException.class)
+	public void cannotUpgradeLevel() {
+		Level[] levels = Level.values();
+		for(Level level : levels) {
+			if(level.nextLevel() != null) continue;
+			user.setLevel(level);
+			user.upgradeLevel();
+		}
+	}
+	
+}
